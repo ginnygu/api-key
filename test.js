6a261f69567ec332e10acd53972ef6b60be97f8a
Ginny Gu Tang <ginnygu@gmail.com>
2020-09-24 12:06:06 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+const key = require('./key.js')
+
+let city = 'new york'
+
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`
+
+fetch(url)
+    .then((data) => data.json())
+    .then((newData) => {
+       console.log(newData)
+    })
